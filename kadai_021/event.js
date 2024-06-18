b76ca271e35cc4ad49c0d2fdb244511f8e6b167e
TomotakaMatsuda <61697097+TomotakaMatsuda@users.noreply.github.com>
2024-06-18 17:17:59 +0900

    Add kadai_021_2

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById('btn');
+const text =  document.getElementById('text');
 btn.addEventListener('click', () => {
     setTimeout(() => {
-        document.getElementById('text').textContent = 'ボタンをクリックしました';
+        text.textContent = 'ボタンをクリックしました';
     }, 2000);
 });
